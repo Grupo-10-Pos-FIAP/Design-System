5245a2d198b9f2d3b0eda936d70afe5be21db4fb
Gabrielle Martins <gabrielle.martiins.96@gmail.com>
2025-09-28 10:32:38 -0300

    refactor(Configs): adjustments indicated in the pull request

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 import { fileURLToPath } from 'node:url';
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+import storybookTest from '@storybook/addon-vitest/vitest-plugin';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
