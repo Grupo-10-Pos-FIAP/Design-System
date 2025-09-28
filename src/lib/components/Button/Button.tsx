5c4c65e799a13274d25aa7487ac4a198d6284c91
Felipe Sampaio <felipesampaio@Felipes-MacBook-Pro.local>
2025-09-28 12:25:37 -0300

    Removido importação do react

--- src/lib/components/Button/Button.tsx
+++ src/lib/components/Button/Button.tsx
@@ -1,15 +1,15 @@
-import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import "./Button.scss"
+import { ButtonHTMLAttributes, forwardRef } from "react"
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  extends ButtonHTMLAttributes<HTMLButtonElement> {
   asChild?: boolean
   variant?: "default" | "outline" | "ghost"
   size?: "sm" | "md" | "lg"
 }
 
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (
     { className = "", variant = "default", size = "md", asChild = false, ...props },
     ref
