313459eba1431f7ff4b2e5b5b4a99df93f15bb36
Felipe Sampaio <felipesampaio@Felipes-MacBook-Pro.local>
2025-09-28 12:18:13 -0300

    Criado componente Header

---
+++ src/lib/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Header.scss";
+
+interface HeaderProps {
+  avatar?: React.ReactNode;
+}
+
+export function Header({ avatar }: HeaderProps) {
+  return (
+    <header className="header">
+      {/* Logo */}
+        <div className="header__logo">
+            <img src="/vite.svg" alt="Logo da empresa" />
+        </div>
+
+        {/* Avatar do usuário */}
+        <div className="header__avatar">{avatar}</div>
+    </header>
+  );
+}
