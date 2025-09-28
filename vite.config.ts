f6afcf247c5caa58999d1401f6c1c97c9c28d6f1
Helen Cris <helencris_morais@hotmail.com>
2025-09-28 17:41:34 -0300

    🐞 fix: fix alias vite, and fix IconName on interface Icon and add joinClassNames

--- vite.config.ts
+++ vite.config.ts
@@ -36,8 +36,9 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@components': path.resolve(__dirname, 'src/components'),
       src: path.resolve(__dirname, 'src'),
+      components: path.resolve(__dirname, 'src/components'),
+      utils: path.resolve(__dirname, 'src/utils'),
     },
   },
   test: {
